9a8f8577a4383adeaecd8228b1bcbf93b2cc3c21
Anton-web13 <85175757+Anton-web13@users.noreply.github.com>
2023-02-18 10:43:47 +0300

    2 Lesson

--- my_react/src/index.js
+++ my_react/src/index.js
@@ -4,14 +4,43 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootDiv = document.getElementById('root');
+
+const root = ReactDOM.createRoot(rootDiv);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
+console.log('srfsdrg');
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+// import Sum from "./utils/someJs";
+
+import sumFromMe from './utils/someJs';
+// //
+// // console.log(sum(1,2));
+//
+// // import {sum, division, multiply} from './utils';
+//
+console.log(sumFromMe(1, 2));
+//
 reportWebVitals();
+
+
+//
+
+
+
+
+
+
+
+
+
+
+
---
+++ my_react/src/utils/someJs.js
@@ -0,0 +1,13 @@
+const sum = (a, b) => a + b;
+
+// const division = (a, b) => a - b;
+//
+// const multiply = (a, b) => a * b;
+
+export default sum;
+
+// export {
+//     sum,
+//     division,
+//     multiply
+// }
